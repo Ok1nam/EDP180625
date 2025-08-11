8411fe22d37a3de9ad2abd9740c8a08662645a58
Ok1nam <linbaaz@yahoo.fr>
2025-08-11 21:20:23 +0200

    suivi subventionukj#

--- client/src/pages/SuiviSubventions.tsx
+++ client/src/pages/SuiviSubventions.tsx
@@ -8,7 +8,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
-import { useToast } from "@/hooks/use-toast";
+import { useToast } from "@/components/ui/use-toast";
 
 interface SubsidyApplication {
   id: string; // Internal unique ID
