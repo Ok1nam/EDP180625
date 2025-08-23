cc261890e62eaafe2a990bf1eb14a19456b63e9b
Ok1nam <linbaaz@yahoo.fr>
2025-08-23 21:11:44 +0200

    on va tester ça comme ça hein

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,8 @@ import Tools from "./pages/Tools";
 import Suivis from "./pages/Suivis";
 import Questionnaire from "./pages/Questionnaire";
 import Calculators from "./pages/Calculators";
+import BusinessPlan from "./pages/BusinessPlan";
+import PartnershipTracker from "./pages/PartnershipTracker";
 import PedagogicalCosts from "./pages/PedagogicalCosts";
 import TrainingPlanner from "./pages/TrainingPlanner";
 import SubsidyGenerator from "@/pages/SubsidyGenerator";
@@ -96,10 +98,12 @@ function MainApplicationContent() {
           <Route path="/suivis" element={<Suivis navigate={navigate} />} />
           <Route path="/suivi-subventions" element={<SuiviSubventions navigate={navigate} />} />
           <Route path="/suivi-prets" element={<SuiviPrets navigate={navigate} />} />
+          <Route path="/partenariats" element={<PartnershipTracker navigate={navigate} />} />
           <Route path="/tableau-bord" element={<Dashboard navigate={navigate} />} />
 
           {/* Documentation & Guides */}
           <Route path="/documentation" element={<DocumentationPage navigate={navigate} />} />
+          <Route path="/methodo" element={<Methodology navigate={navigate} />} />
           <Route path="/cartographie" element={<LocationAnalysis navigate={navigate} />} />
           <Route path="/organigramme" element={<Organigramme navigate={navigate} />} />
           <Route path="/entretiens" element={<Entretiens navigate={navigate} />} />
@@ -110,9 +114,14 @@ function MainApplicationContent() {
           <Route path="/contact-et-aide" element={<ContactEtAide navigate={navigate} />} />
           <Route path="/expert-comptable" element={<Contact navigate={navigate} />} />
           <Route path="/edp" element={<EcoleDeProduction navigate={navigate} />} />
+          <Route path="/faq" element={<UnderDevelopment title="Foire Aux Questions (FAQ)" navigate={navigate} />} />
+          <Route path="/support" element={<UnderDevelopment title="Support Technique" navigate={navigate} />} />
           <Route path="/contact" element={<Contact navigate={navigate} />} />
 
-
+          {/* Routes pour les pages qui étaient gérées par currentPage mais qui n'ont pas de lien direct dans le menu. */}
+          <Route path="/business-plan" element={<BusinessPlan navigate={navigate} />} />
+          <Route path="/couts-pedagogiques" element={<PedagogicalCosts navigate={navigate} />} />
+          <Route path="/planification" element={<TrainingPlanner navigate={navigate} />} />
 
           {/* Route par défaut (404 ou redirige vers l'accueil) */}
           <Route path="*" element={<Home navigate={navigate} />} />
